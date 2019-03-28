de7f7c6e7d775b5aa65a552bde5a1b5e3dd22a54
Girijashankar Senapati <gshankar165@gmail.com>
2019-03-29 00:03:27 +0530

    Add files via upload

---
+++ exec_code.py
@@ -0,0 +1,11 @@
+
+
+import sys
+
+
+#copy both the .py file in a folder and replace the folder path in below box. 
+#Note: do not remove *r before the path 
+sys.path.append(r"E:\Code_test\Render_Region_Tool")
+
+from MtoA_Region_Render_Tool import *
+tool_window_func()
